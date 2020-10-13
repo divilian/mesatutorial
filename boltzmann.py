b31727297971c8cc8fd9a72c70bb140eea0e0039
Stephen <stephen@umw.edu>
2020-10-13 17:02:18 -0400

    Refactor for Grid subclasses. Delete images properly.

--- boltzmann.py
+++ boltzmann.py
@@ -10,6 +10,7 @@ import logging
 import time
 import subprocess
 import sys
+import glob
 
 
 logging.basicConfig(level=logging.WARNING)
@@ -23,7 +24,7 @@ def compute_gini(model):
 
 def extract_agent_wealth(model):
     return pd.Series([ a.wealth for a in model.schedule.agents ])
-    
+
 
 class MoneyAgent(Agent):
 
@@ -34,23 +35,25 @@ class MoneyAgent(Agent):
     def step(self):
         logging.info("Running agent {}! (currently ${})".format(
             self.unique_id, self.wealth))
-        if self.wealth == 0:
-            return
+        if self.wealth > 0:
+            self.give_money()
+
+    def give_money(self):
         other = self.random.choice(self.model.schedule.agents)
         if self == other:
             logging.info("Trading with myself!")
         other.wealth += 1
         self.wealth -= 1
-        
+
 
 class MoneyModel(Model):
 
-    def __init__(self, N):
+    def __init__(self, N, agent_class=MoneyAgent):
         self.num_agents = N
         self.schedule = RandomActivation(self)
         self.num_steps = 0
         for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
+            a = agent_class(i, self)
             self.schedule.add(a)
         self.datacollector = DataCollector(
             agent_reporters={"agent_wealth": "wealth"})
@@ -67,50 +70,54 @@ class MoneyModel(Model):
             self.step()
 
 
-if len(sys.argv) <= 1:
-    print("Usage: boltzmann.py single|batch.")
-    sys.exit(1)
 
-if sys.argv[1] == "single":
+if __name__ == "__main__":
 
-    # Single simulation run.
-    m = MoneyModel(200)
-    m.run(100)
+    if len(sys.argv) <= 1:
+        print("Usage: boltzmann.py single|batch.")
+        sys.exit(1)
 
-    wealths = m.datacollector.get_agent_vars_dataframe()
-    max_wealth_ever = wealths.agent_wealth.max()
-    max_iter = wealths.index.get_level_values("Step").max()
-    subprocess.run("rm -f /tmp/wealth0??.png /tmp/animation.gif".split(" "))
-    for step in wealths.index.get_level_values("Step").unique():
-        wealths.xs(step, level="Step").plot(kind="hist", density=True,
-            bins=range(0,max_wealth_ever+1))
-        plt.title("Iteration {} of {}".format(step+1, max_iter+1))
-        plt.savefig("/tmp/wealth{:03d}.png".format(step))
+    if sys.argv[1] == "single":
+
+        # Single simulation run.
+        m = MoneyModel(100)
+        m.run(50)
+
+        wealths = m.datacollector.get_agent_vars_dataframe()
+        max_wealth_ever = wealths.agent_wealth.max()
+        max_iter = wealths.index.get_level_values("Step").max()
+        files_to_delete = glob.glob("/tmp/wealth0??.png")
+        subprocess.run(["rm", "-f"] + files_to_delete + ["/tmp/animation.gif"])
+        for step in wealths.index.get_level_values("Step").unique():
+            wealths.xs(step, level="Step").plot(kind="hist", density=True,
+                bins=range(0,max_wealth_ever+1))
+            plt.title("Iteration {} of {}".format(step+1, max_iter+1))
+            plt.savefig("/tmp/wealth{:03d}.png".format(step))
+            plt.close()
+        subprocess.run("convert -delay 15 -loop 1 /tmp/wealth0??.png /tmp/animation.gif".split(" "))
+        print("Output in /tmp/animation.gif.")
+
+    else:
+
+        # Batch simulation run.
+        fixed_params = {}
+        variable_params = {"N": range(5,100) }
+
+        batch_run = BatchRunner(MoneyModel,
+            variable_params,
+            fixed_params,
+            iterations=10,
+            max_steps=50,
+            model_reporters={"Gini": compute_gini})
+
+        batch_run.run_all()
+
+        run_data = batch_run.get_model_vars_dataframe()
+        run_data.head()
+        plt.scatter(run_data.N, run_data.Gini)
+        plt.ylim((0,1))
+        plt.xlabel("Number of agents")
+        plt.ylabel("Gini coefficient")
+        plt.savefig("/tmp/Gini_scatter.png")
         plt.close()
-    subprocess.run("convert -delay 15 -loop 1 /tmp/wealth0??.png /tmp/animation.gif".split(" "))
-    print("Output in /tmp/animation.gif.")
-
-else:
-
-    # Batch simulation run.
-    fixed_params = {} 
-    variable_params = {"N": range(5,100) }
-
-    batch_run = BatchRunner(MoneyModel,
-        variable_params,
-        fixed_params,
-        iterations=10,
-        max_steps=50,
-        model_reporters={"Gini": compute_gini})
-
-    batch_run.run_all()
-
-    run_data = batch_run.get_model_vars_dataframe()
-    run_data.head()
-    plt.scatter(run_data.N, run_data.Gini)
-    plt.ylim((0,1))
-    plt.xlabel("Number of agents")
-    plt.ylabel("Gini coefficient")
-    plt.savefig("/tmp/Gini_scatter.png")
-    plt.close()
-    print("Output in /tmp/Gini_scatter.png.")
+        print("Output in /tmp/Gini_scatter.png.")
