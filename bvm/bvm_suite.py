d872b481a9b1a94295cde0987936e13ad656c048
Stephen <stephen@umw.edu>
2021-02-17 19:45:59 -0500

    Add status message.

--- bvm/bvm_suite.py
+++ bvm/bvm_suite.py
@@ -1,10 +1,13 @@
 
+# Things I don't know how to get Mesa to do out of the box:
+# - a "double slider" (or whatever it's called) in UserSettableParameter
+
 import numpy as np
 import pandas as pd
 from mesa import Agent, Model
 from mesa.datacollection import DataCollector
 from mesa.batchrunner import BatchRunner
-from mesa.visualization.modules import NetworkModule
+from mesa.visualization.modules import NetworkModule, TextElement
 from StaticChartVisualization import StaticChartModule
 from mesa.visualization.UserParam import UserSettableParameter
 from mesa.visualization.ModularVisualization import ModularServer
@@ -14,7 +17,7 @@ from bvm import VoterAgent, SocialWorld, Opinion, iters_to_converge
 
 class SocialWorldSuite(Model):
 
-    def __init__(self, N1, N2, p, suite_size):
+    def __init__(self, N1, N2, Nstep, p, suite_size):
 
         logging.info("Initializing SocialWorldSuite({},{},{})...".format(
             N1,N2,p))
@@ -22,17 +25,27 @@ class SocialWorldSuite(Model):
         self.p = p
         self.N1 = N1
         self.N2 = N2
+        self.Nstep = Nstep
+        self.msg = "Ready"
         self.suite_size = suite_size
         self.running = True
         self.df = pd.DataFrame({"N":[],"itersToConverge":[]})
+        self.msg_printed = False
 
 
     def step(self):
 
+        param_vals = np.arange(self.N1, self.N2+self.Nstep, self.Nstep)
         self.running = True
+        if not self.msg_printed:
+            self.msg=("Running {} sims for each of {} param values " +
+                "({} total)...").format(self.suite_size, 
+                len(param_vals), self.suite_size * len(param_vals))
+            self.msg_printed = True
+            return
 
         fixed = {"p":self.p}
-        variable = {"N": np.arange(self.N1,self.N2,10)}
+        variable = {"N": param_vals}
 
         batch_run = BatchRunner(SocialWorld, variable, fixed,
             iterations=self.suite_size, max_steps=1000,
@@ -40,6 +53,7 @@ class SocialWorldSuite(Model):
                 "itersToConverge": iters_to_converge})
         batch_run.run_all()
         self.running = False
+        self.msg="Done."
         self.df = batch_run.get_model_vars_dataframe()
 
 
@@ -48,18 +62,27 @@ class SocialWorldSuite(Model):
             self.step()
 
 
+class StatusMessage(TextElement):
+    def render(self, model):
+        return model.msg
+
+
 if __name__ == "__main__":
 
-    N1 = UserSettableParameter("slider","Number of agents (low)",10,1,100,1)
-    N2 = UserSettableParameter("slider","Number of agents (high)",140,1,200,1)
+    N1 = UserSettableParameter("slider","Number of agents (low)",10,10,100,10)
+    N2 = UserSettableParameter("slider","Number of agents (high)",100,10,200,10)
+    Nstep = UserSettableParameter("slider","Number of agents (step)",10,1,20,1)
 
     p = UserSettableParameter("slider","ER edge probability (p)",.2,0.05,1,.05)
+
+    msg = StatusMessage()
 
     suite_size = UserSettableParameter("slider","Suite size",20,5,100,1)
 
     time_to_converge = StaticChartModule(name="Average iterations to converge")
 
-    server = ModularServer(SocialWorldSuite, [time_to_converge],
-        "BVM", { "N1":N1, "N2":N2, "p":p, "suite_size":suite_size })
+    server = ModularServer(SocialWorldSuite, [time_to_converge, msg],
+        "BVM", { "N1":N1, "N2":N2, "Nstep":Nstep, "p":p,
+         "suite_size":suite_size })
     server.port = 8081
     server.launch()
